86a786949e0d6514d81ac1ad48e29859f023f3f2
Tobias Lønnerød Madsen <m@dsen.tv>
2019-07-05 15:46:29 +0200

    Added sample along with some integration tests
    Signed-off-by: Tobias Lønnerød Madsen <m@dsen.tv>

---
+++ samples/CloudNative.CloudEvents.AspNetCoreSample/Controllers/CloudEventController.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Cloud Native Foundation. 
+// Licensed under the Apache 2.0 license.
+// See LICENSE file in the project root for full license information.
+
+namespace CloudNative.CloudEvents.AspNetCoreSample.Controllers
+{
+    using System.Collections.Generic;
+    using CloudNative.CloudEvents;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/events")]
+    [ApiController]
+    public class CloudEventController : ControllerBase
+    {
+        [HttpPost("receive")]
+        public ActionResult<IEnumerable<string>> ReceiveCloudEvent([FromBody] CloudEvent cloudEvent)
+        {
+            return Ok($"Received event with ID {cloudEvent.Id}");
+        }
+    }
+}
---
+++ test/CloudNative.CloudEvents.IntegrationTests/AspNetCore/CloudEventControllerTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Cloud Native Foundation. 
+// Licensed under the Apache 2.0 license.
+// See LICENSE file in the project root for full license information.
+
+namespace CloudNative.CloudEvents.IntegrationTests.AspNetCore
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+    using CloudNative.CloudEvents.AspNetCoreSample;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Xunit;
+
+    public class CloudEventControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public CloudEventControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Controller_WithValidCloudEvent_DeserializesUsingPipeline()
+        {
+            // Arrange
+            var cloudEvent = new CloudEvent("test-type", new Uri("urn:integration-tests"))
+            {
+                Id = Guid.NewGuid().ToString(),
+            };
+            var content = new CloudEventContent(cloudEvent, ContentMode.Structured, new JsonEventFormatter());
+
+            // Act
+            var result = await _factory.CreateClient().PostAsync("/api/events/receive", content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.Contains(cloudEvent.Id, await result.Content.ReadAsStringAsync());
+        }
+    }
+}
