d08756490ea6f3a60474555e5aa354a82f0b4566
Jon Skeet <jonskeet@google.com>
2020-06-03 08:27:32 +0100

    Remove internal access to CloudEvents from other projects
    InternalsVisibleTo can be very difficult to use correctly between production packages - it's easy to forget that an "internal" member can't take any breaking changes due to other packages that depend on it.
    It's reasonable to use InternalsVisibleTo for test packages though.
    
    Signed-off-by: Jon Skeet <jonskeet@google.com>

--- src/CloudNative.CloudEvents/Attributes.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Cloud Native Foundation. 
-// Licensed under the Apache 2.0 license.
-// See LICENSE file in the project root for full license information.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("CloudNative.CloudEvents.Amqp")]
-[assembly: InternalsVisibleTo("CloudNative.CloudEvents.Mqtt")]
-[assembly: InternalsVisibleTo("CloudNative.CloudEvents.Kafka")]
