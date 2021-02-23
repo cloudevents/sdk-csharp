368811c23bdc4cc17ad06cfa4122cb9652c00e8c
Jon Skeet <jonskeet@google.com>
2021-02-23 15:42:21 +0000

    Comprehensive tests for JsonEventFormatter
    This required a bit of rework for the implementation, but the result
    is cleaner, in my view.
    
    There's one skipped test, for when an attribute (other than data) is
    populated via an integer/Boolean token. We should discuss what we
    want to happen in that case.
    
    The extension tests had to change as JsonEventFormatter now
    validates that the event it's parsed is valid (for structured mode),
    and the ones we were using didn't have a source attribute.
    
    (We will probably want to pepper lots of places with calls to
    CloudEvent.Validate, but most of those can come later.)
    
    Signed-off-by: Jon Skeet <jonskeet@google.com>

--- test/CloudNative.CloudEvents.UnitTests/TestHelpers.cs
+++ test/CloudNative.CloudEvents.UnitTests/TestHelpers.cs
@@ -3,6 +3,7 @@
 // See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using Xunit;
 
@@ -11,8 +12,82 @@ namespace CloudNative.CloudEvents.UnitTests
     /// <summary>
     /// Helpers for test code, e.g. common non-trivial assertions.
     /// </summary>
-    internal class TestHelpers
+    internal static class TestHelpers
     {
+        /// <summary>
+        /// A set of extension attributes covering all attributes types.
+        /// The name of each attribute is the lower-cased form of the attribute type
+        /// name, with all punctuation removed (e.g. "urireference").
+        /// The attributes do not have any extra validation.
+        /// </summary>
+        internal static IEnumerable<CloudEventAttribute> AllTypesExtensions { get; } = new List<CloudEventAttribute>
+        {
+            CloudEventAttribute.CreateExtension("binary", CloudEventAttributeType.Binary),
+            CloudEventAttribute.CreateExtension("boolean", CloudEventAttributeType.Boolean),
+            CloudEventAttribute.CreateExtension("integer", CloudEventAttributeType.Integer),
+            CloudEventAttribute.CreateExtension("string", CloudEventAttributeType.String),
+            CloudEventAttribute.CreateExtension("timestamp", CloudEventAttributeType.Timestamp),
+            CloudEventAttribute.CreateExtension("uri", CloudEventAttributeType.Uri),
+            CloudEventAttribute.CreateExtension("urireference", CloudEventAttributeType.UriReference)
+        }.AsReadOnly();
+
+        /// <summary>
+        /// Arbitrary binary data to be used for testing.
+        /// Calling code should not rely on the exact value.
+        /// </summary>
+        internal static byte[] SampleBinaryData { get; } = new byte[] { 1, 2, 3 };
+
+        /// <summary>
+        /// The base64 representation of <see cref="SampleBinaryData"/>.
+        /// </summary>
+        internal static string SampleBinaryDataBase64 { get; } = Convert.ToBase64String(SampleBinaryData);
+
+        /// <summary>
+        /// Arbitrary timestamp to be used for testing.
+        /// Calling code should not rely on the exact value.
+        /// </summary>
+        internal static DateTimeOffset SampleTimestamp { get; } = new DateTimeOffset(2021, 2, 19, 12, 34, 56, 789, TimeSpan.FromHours(1));
+
+        /// <summary>
+        /// The RFC 3339 text representation of <see cref="SampleTimestamp"/>.
+        /// </summary>
+        internal static string SampleTimestampText { get; } = "2021-02-19T12:34:56.789+01:00";
+
+        /// <summary>
+        /// Arbitrary absolute URI to be used for testing.
+        /// Calling code should not rely on the exact value.
+        /// </summary>
+        internal static Uri SampleUri { get; } = new Uri("https://absoluteuri/path");
+
+        /// <summary>
+        /// The textual representation of <see cref="SampleUri"/>.
+        /// </summary>
+        internal static string SampleUriText { get; } = "https://absoluteuri/path";
+
+        /// <summary>
+        /// Arbitrary absolute URI to be used for testing.
+        /// Calling code should not rely on the exact value.
+        /// </summary>
+        internal static Uri SampleUriReference { get; } = new Uri("//urireference/path", UriKind.RelativeOrAbsolute);
+
+        /// <summary>
+        /// The textual representation of <see cref="SampleUriReference"/>.
+        /// </summary>
+        internal static string SampleUriReferenceText { get; } = "//urireference/path";
+
+        /// <summary>
+        /// Populates a CloudEvent with minimal valid attribute values.
+        /// Calling code should not take a dependency on the exact values used.
+        /// </summary>
+        /// <returns>The original CloudEvent reference, for method chaining purposes.</returns>
+        internal static CloudEvent PopulateRequiredAttributes(this CloudEvent cloudEvent)
+        {
+            cloudEvent.Id = "test-id";
+            cloudEvent.Source = new Uri("//test", UriKind.RelativeOrAbsolute);
+            cloudEvent.Type = "test-id";
+            return cloudEvent;
+        }
+
         /// <summary>
         /// Asserts that two timestamp values are equal, expressing the expected value as a
         /// string for compact testing.
