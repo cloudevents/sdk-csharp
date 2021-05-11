3f35bfd5a6995d49ee8215cdca3d6ac20fcabfdb
Jon Skeet <jonskeet@google.com>
2021-05-11 09:33:26 +0100

    Implement JsonEventFormatter<T> for Newtonsoft.Json
    Signed-off-by: Jon Skeet <jonskeet@google.com>

---
+++ test/CloudNative.CloudEvents.UnitTests/NewtonsoftJson/GenericJsonEventFormatterTest.cs
@@ -0,0 +1,167 @@
+﻿// Copyright 2021 Cloud Native Foundation. 
+// Licensed under the Apache 2.0 license.
+// See LICENSE file in the project root for full license information.
+
+using CloudNative.CloudEvents.UnitTests;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Mime;
+using System.Text;
+using Xunit;
+
+namespace CloudNative.CloudEvents.NewtonsoftJson.UnitTests
+{
+    /// <summary>
+    /// Tests for <see cref="JsonEventFormatter{T}"/>
+    /// </summary>
+    public class GenericJsonEventFormatterTest
+    {
+        [Fact]
+        public void DecodeStructuredMode()
+        {
+            var obj = JsonEventFormatterTest.CreateMinimalValidJObject();
+            obj["data"] = new JObject { [AttributedModel.JsonPropertyName] = "test" };
+            byte[] bytes = Encoding.UTF8.GetBytes(obj.ToString());
+
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var cloudEvent = formatter.DecodeStructuredModeMessage(bytes, null, null);
+
+            var model = (AttributedModel) cloudEvent.Data;
+            Assert.Equal("test", model.AttributedProperty);
+        }
+
+        [Fact]
+        public void DecodeStructuredMode_ContentTypeIgnored()
+        {
+            var obj = JsonEventFormatterTest.CreateMinimalValidJObject();
+            obj["data"] = new JObject { [AttributedModel.JsonPropertyName] = "test" };
+            byte[] bytes = Encoding.UTF8.GetBytes(obj.ToString());
+
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var cloudEvent = formatter.DecodeStructuredModeMessage(bytes, new ContentType("text/plain"), null);
+
+            var model = (AttributedModel)cloudEvent.Data;
+            Assert.Equal("test", model.AttributedProperty);
+        }
+
+        [Fact]
+        public void DecodeStructuredMode_NoData()
+        {
+            var obj = JsonEventFormatterTest.CreateMinimalValidJObject();
+            byte[] bytes = Encoding.UTF8.GetBytes(obj.ToString());
+
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var cloudEvent = formatter.DecodeStructuredModeMessage(bytes, null, null);
+            Assert.Null(cloudEvent.Data);
+        }
+
+        [Fact]
+        public void DecodeStructuredMode_Base64Data()
+        {
+            var obj = JsonEventFormatterTest.CreateMinimalValidJObject();
+            obj["data_base64"] = Convert.ToBase64String(Encoding.UTF8.GetBytes("{}"));
+            byte[] bytes = Encoding.UTF8.GetBytes(obj.ToString());
+
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            Assert.Throws<ArgumentException>(() => formatter.DecodeStructuredModeMessage(bytes, null, null));
+        }
+
+        [Fact]
+        public void DecodeBinaryEventModeData()
+        {
+            var obj = new JObject { [AttributedModel.JsonPropertyName] = "test" };
+            byte[] bytes = Encoding.UTF8.GetBytes(obj.ToString());
+
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var cloudEvent = new CloudEvent();
+            formatter.DecodeBinaryModeEventData(bytes, cloudEvent);
+
+            var model = (AttributedModel)cloudEvent.Data;
+            Assert.Equal("test", model.AttributedProperty);
+        }
+
+        [Fact]
+        public void DecodeBinaryEventModeData_NoData()
+        {
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var cloudEvent = new CloudEvent { Data = "original" };
+            formatter.DecodeBinaryModeEventData(new byte[0], cloudEvent);
+            Assert.Null(cloudEvent.Data);
+        }
+
+        [Fact]
+        public void EncodeStructuredMode()
+        {
+            var cloudEvent = new CloudEvent().PopulateRequiredAttributes();
+            cloudEvent.Data = new AttributedModel { AttributedProperty = "test" };
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var body = formatter.EncodeStructuredModeMessage(cloudEvent, out _);
+            var jobject = JsonEventFormatterTest.ParseJson(body);
+            Assert.False(jobject.ContainsKey("data_base64"));
+            var data = (JObject)jobject["data"];
+
+            new JTokenAsserter
+            {
+                { AttributedModel.JsonPropertyName, JTokenType.String, "test" }
+            }.AssertProperties(data, assertCount: true);
+        }
+
+        [Fact]
+        public void EncodeStructuredMode_NoData()
+        {
+            var cloudEvent = new CloudEvent().PopulateRequiredAttributes();
+
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var body = formatter.EncodeStructuredModeMessage(cloudEvent, out _);
+            var jobject = JsonEventFormatterTest.ParseJson(body);
+            Assert.False(jobject.ContainsKey("data"));
+            Assert.False(jobject.ContainsKey("data_base64"));
+        }
+
+        [Fact]
+        public void EncodeStructuredMode_WrongType()
+        {
+            var cloudEvent = new CloudEvent().PopulateRequiredAttributes();
+            cloudEvent.Data = new OtherModelClass { Text = "Wrong type" };
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            Assert.Throws<InvalidCastException>(() => formatter.EncodeStructuredModeMessage(cloudEvent, out _));
+        }
+
+        [Fact]
+        public void EncodeBinaryModeEventData()
+        {
+            var cloudEvent = new CloudEvent().PopulateRequiredAttributes();
+            cloudEvent.Data = new AttributedModel { AttributedProperty = "test" };
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            var body = formatter.EncodeBinaryModeEventData(cloudEvent);
+            var jobject = JsonEventFormatterTest.ParseJson(body);
+
+            new JTokenAsserter
+            {
+                { AttributedModel.JsonPropertyName, JTokenType.String, "test" }
+            }.AssertProperties(jobject, assertCount: true);
+        }
+
+        [Fact]
+        public void EncodeBinaryModeEventData_NoData()
+        {
+            var cloudEvent = new CloudEvent().PopulateRequiredAttributes();
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            Assert.Empty(formatter.EncodeBinaryModeEventData(cloudEvent));
+        }
+
+        [Fact]
+        public void EncodeBinaryModeEventData_WrongType()
+        {
+            var cloudEvent = new CloudEvent().PopulateRequiredAttributes();
+            cloudEvent.Data = new OtherModelClass { Text = "Wrong type" };
+            var formatter = CloudEventFormatterAttribute.CreateFormatter(typeof(AttributedModel));
+            Assert.Throws<InvalidCastException>(() => formatter.EncodeBinaryModeEventData(cloudEvent));
+        }
+
+        private class OtherModelClass
+        {
+            public string Text { get; set; }
+        }
+    }
+}
--- test/CloudNative.CloudEvents.UnitTests/NewtonsoftJson/JsonEventFormatterTest.cs
+++ test/CloudNative.CloudEvents.UnitTests/NewtonsoftJson/JsonEventFormatterTest.cs
@@ -6,7 +6,6 @@ using CloudNative.CloudEvents.UnitTests;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Mime;
@@ -945,45 +944,6 @@ namespace CloudNative.CloudEvents.NewtonsoftJson.UnitTests
             byte[] bytes = Encoding.UTF8.GetBytes(array.ToString());
             var formatter = new JsonEventFormatter();
             return formatter.DecodeBatchModeMessage(bytes, s_jsonCloudEventContentType, null);
-        }
-
-        private class JTokenAsserter : IEnumerable
-        {
-            private readonly List<(string name, JTokenType type, object value)> expectations = new List<(string, JTokenType, object)>();
-
-            // Just for collection initializers
-            public IEnumerator GetEnumerator() => throw new NotImplementedException();
-
-            public void Add<T>(string name, JTokenType type, T value) =>
-                expectations.Add((name, type, value));
-
-            public void AssertProperties(JObject obj, bool assertCount)
-            {
-                foreach (var expectation in expectations)
-                {
-                    Assert.True(
-                        obj.TryGetValue(expectation.name, out var token),
-                        $"Expected property '{expectation.name}' to be present");
-                    Assert.Equal(expectation.type, token.Type);
-                    // No need to check null values, as they'll have a null token type.
-                    if (expectation.value is object)
-                    {
-                        Assert.Equal(expectation.value, token.ToObject(expectation.value.GetType()));
-                    }
-                }
-                if (assertCount)
-                {
-                    Assert.Equal(expectations.Count, obj.Count);
-                }
-            }
-        }
-
-        private class AttributedModel
-        {
-            public const string JsonPropertyName = "customattribute";
-
-            [JsonProperty(JsonPropertyName)]
-            public string AttributedProperty { get; set; }
         }
     }
 }
