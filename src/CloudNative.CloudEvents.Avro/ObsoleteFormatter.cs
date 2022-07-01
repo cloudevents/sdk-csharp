d6414579a0b75892f8737b33492de21896947e9c
Jon Skeet <jonskeet@google.com>
2022-07-01 16:01:48 +0100

    fix: Put AvroEventFormatter in the right namespace
    We still need it in CloudNative.CloudEvents for backward
    compatibility, but we can do that via derivation - which is
    pleasantly simple given that the only constructor is parameterless.
    
    Users should update to use the CloudNative.CloudEvents.Avro
    namespace at their earliest convenience.
    
    Fixes #219.
    
    Signed-off-by: Jon Skeet <jonskeet@google.com>

---
+++ src/CloudNative.CloudEvents.Avro/ObsoleteFormatter.cs
@@ -0,0 +1,20 @@
+﻿// Copyright 2021 Cloud Native Foundation. 
+// Licensed under the Apache 2.0 license.
+// See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace CloudNative.CloudEvents;
+
+/// <summary>
+/// Formatter that implements the Avro Event Format.
+/// </summary>
+/// <remarks>
+/// This class is the wrong namespace, and is only present for backward compatibility reasons.
+/// Please use CloudNative.CloudEvents.Avro.AvroEventFormatter instead
+/// (which this class derives from for convenience).
+/// </remarks>
+[Obsolete("This class is the wrong namespace, and is only present for backward compatibility reasons. Please use CloudNative.CloudEvents.Avro.AvroEventFormatter.")]
+public class AvroEventFormatter : Avro.AvroEventFormatter
+{
+}
