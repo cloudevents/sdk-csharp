ea43bdb53877c85c27e756c86d70ffad34a1c8da
Jon Skeet <jonskeet@google.com>
2023-03-06 15:12:31 +0000

    Regenerate protobuf with the right filename
    For some reason we were renaming cloudevents.proto as ProtoSchema.proto, which breaks compatibility with anyone using the original file.
    
    Renaming the reflection class is a breaking change, so I've added an obsolete compatibility class to keep compatibility.
    Also updated to Google.Protobuf 3.22.0 (of the general protobuf 22.0 release; protobuf versioning is complex).
    
    Fixes #256
    
    Signed-off-by: Jon Skeet <jonskeet@google.com>

---
+++ src/CloudNative.CloudEvents.Protobuf/ProtoSchemaReflection.cs
@@ -0,0 +1,23 @@
+﻿// Copyright 2023 Cloud Native Foundation.
+// Licensed under the Apache 2.0 license.
+// See LICENSE file in the project root for full license information.
+
+using CloudNative.CloudEvents.V1;
+using Google.Protobuf.Reflection;
+using System;
+
+namespace CloudNative.CloudEvents.V1;
+
+/// <summary>
+/// Access to reflection information for the CloudEvents protobuf schema.
+/// </summary>
+/// <remarks>
+/// This class exists for backward-compatibility, when the protobuf messages
+/// were generated with a file named ProtoSchema.proto instead of cloudevents.proto.
+/// </remarks>
+[Obsolete($"Use {nameof(CloudeventsReflection)} instead.")]
+public class ProtoSchemaReflection
+{
+    /// <summary>File descriptor for cloudevents.proto</summary>
+    public static FileDescriptor Descriptor => CloudeventsReflection.Descriptor;
+}
---
+++ test/CloudNative.CloudEvents.UnitTests/Protobuf/CompatibilityTest.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2023 Cloud Native Foundation. 
+// Licensed under the Apache 2.0 license.
+// See LICENSE file in the project root for full license information.
+
+using CloudNative.CloudEvents.V1;
+using Xunit;
+
+namespace CloudNative.CloudEvents.UnitTests.Protobuf;
+
+public class CompatibilityTest
+{
+    [Fact]
+    public void ProtoSchemaReflectionEquivalence()
+    {
+#pragma warning disable CS0618 // Type or member is obsolete
+        Assert.Same(CloudeventsReflection.Descriptor, ProtoSchemaReflection.Descriptor);
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
